ce47dc1ab24e5b36b36ebc9c79fcbd6f5212ad79
Siddamsetti Santosh <santoshsiddamsetti0507@gmail.com>
2026-06-13 11:45:45 +0530

    Recap of basics python

---
+++ Basics/basics.py
@@ -0,0 +1,60 @@
+# Variables in Python
+
+name = "Santosh"
+age = 21
+cgpa = 9.5
+is_single = True
+
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Placed:", is_single)
+
+# Common Data Types
+
+n1 = 100
+n2 = 99.99
+language = "Python"
+flag = True
+
+print(type(n1))
+print(type(n2))
+print(type(language))
+print(type(flag))
+
+# Arithmetic Operators
+
+a = 15
+b = 4
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+
+# Type Casting
+
+age = "21"
+
+print(type(age))
+
+age = int(age)
+
+print(type(age))
+
+height = 5.8
+
+height = int(height)
+
+print(height)
+
+# Taking Input from User
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("Your age is", age)
